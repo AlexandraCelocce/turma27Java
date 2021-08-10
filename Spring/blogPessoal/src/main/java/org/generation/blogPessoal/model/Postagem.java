801e3284a279b37bc8f1854a44552c86ce06cb20
AlexandraCelocce <alexandra.ms@outlook.com>
2021-08-09 22:24:27 -0300

    J-unit

--- Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -1,47 +1,54 @@
 package org.generation.blogPessoal.model;
 
-import java.util.Date;
-
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity
-@Table(name = "tb_postagens")
+@Entity 
+@Table(name = "postagens") 
+
 public class Postagem {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private long id; 
 
-	@NotNull(message = "O atributo título é obrigatório")
-	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
-	private String titulo;
+	@NotNull 
+	@Size(min = 5, max = 100)
+	private String titulo; 
 
-	@NotNull(message = "O atributo texto é obrigatório")
-	@Size(min = 10, max = 500, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
+	@NotNull // @NotBlank
+	@Size(min = 10, max = 500)
 	private String texto;
 
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date data = new java.sql.Date(System.currentTimeMillis());
+	@UpdateTimestamp
+	private LocalDate data; 
 	
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-		
+	
 	@ManyToOne
 	@JsonIgnoreProperties ("postagem")
 	private Usuario usuario;
 	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public long getId() {
 		return id;
 	}
@@ -66,14 +73,14 @@ public class Postagem {
 		this.texto = texto;
 	}
 
-	public Date getData() {
+	public LocalDate getData() {
 		return data;
 	}
 
-	public void setData(Date data) {
+	public void setData(LocalDate data) {
 		this.data = data;
 	}
-	
+
 	public Tema getTema() {
 		return tema;
 	}
@@ -82,12 +89,4 @@ public class Postagem {
 		this.tema = tema;
 	}
 
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-	
 }
--- Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,42 +1,40 @@
 package org.generation.blogPessoal.model;
+
 import java.time.LocalDate;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+//import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "tb_usuarios")
+@Table (name = "tb_usuario")
 public class Usuario {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull(message = "O atributo nome é obrigatório")
-	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	@NotBlank
+	@Size(min=2, max=35)
 	private String nome;
 	
-	@NotNull(message = "O atributo usuário é obrigatório")
-	@NotBlank(message = "O atributo usuário não pode ser vazio")
-	@Email(message = "O atributo usuário deve ser um email")
+	@NotBlank
+	@Size(min=5, max=75)
 	private String usuario;
 	
-	@NotNull(message = "O atributo senha é obrigatório")
-	@Size(min = 8, message = "O atributo senha deve ter no mínimo 8 caracteres")
+	@NotBlank
+	@Size(min=2,max=1000)
 	private String senha;
 	
 	@Column(name = "dt_nascimento")
@@ -47,23 +45,8 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List <Postagem> postagem;
 
-	// Primeiro método Construtor
-
-	public Usuario(long id, String nome, String usuario, String senha, LocalDate dataNascimento) {
-		this.id = id;
-		this.nome = nome;
-		this.usuario = usuario;
-		this.senha = senha;
-		this.dataNascimento = dataNascimento;
-	}
-
-	// Segundo método Construtor
-
-	public Usuario() {	}
-
-
 	public long getId() {
-		return this.id;
+		return id;
 	}
 
 	public void setId(long id) {
@@ -71,7 +54,7 @@ public class Usuario {
 	}
 
 	public String getNome() {
-		return this.nome;
+		return nome;
 	}
 
 	public void setNome(String nome) {
@@ -79,7 +62,7 @@ public class Usuario {
 	}
 
 	public String getUsuario() {
-		return this.usuario;
+		return usuario;
 	}
 
 	public void setUsuario(String usuario) {
@@ -87,15 +70,15 @@ public class Usuario {
 	}
 
 	public String getSenha() {
-		return this.senha;
+		return senha;
 	}
 
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
+	
 	public LocalDate getDataNascimento() {
-		return this.dataNascimento;
+		return dataNascimento;
 	}
 
 	public void setDataNascimento(LocalDate dataNascimento) {
@@ -103,7 +86,7 @@ public class Usuario {
 	}
 
 	public List<Postagem> getPostagem() {
-		return this.postagem;
+		return postagem;
 	}
 
 	public void setPostagem(List<Postagem> postagem) {
